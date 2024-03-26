dd8b88281467a53eebb2cc12165dda6a939da0d1
Sasipa <sasipa.ratt@bumail.net>
2024-03-26 10:17:09 +0700

    Summary

---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScoreManager : MonoBehaviour
+{
+    public static int score;
+    public Text scoreText;
+    
+    void Start()
+    {
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "SCORE : " + score;
+    }
+}
